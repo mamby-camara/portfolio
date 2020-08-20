7ec13184983c1580da0bd34f334a7debb0aec257
camamby <camamby@gmail.com>
2020-08-20 22:52:22 +0200

    Push last version

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ To begin using this template, choose one of the following options to get started
 * [Fork, Clone, or Download on GitHub](https://github.com/BlackrockDigital/startbootstrap-freelancer)
 
 ## Usage
-Site URL (local xampp) : http://localhost/portfolio/index.php
+
 ### Basic Usage
 
 After downloading, simply edit the HTML and CSS files included with the template in a code editor to make changes. These are the only files you need to worry about, you can ignore everything else! To preview the changes you make to the code, you can open the `index.html` file in your web browser.
@@ -50,7 +50,10 @@ Have a bug or an issue with this template? [Open a new issue](https://github.com
 
 ## About
 
-Site de Mamby
+Start Bootstrap is an open source library of free Bootstrap templates and themes. All of the free templates and themes on Start Bootstrap are released under the MIT license, which means you can use them for any purpose, even for commercial projects.
+
+* https://startbootstrap.com
+* https://twitter.com/SBootstrap
 
 Start Bootstrap was created by and is maintained by **[David Miller](http://davidmiller.io/)**, Owner of [Blackrock Digital](http://blackrockdigital.io/).
 
--- pages/accueil.php
+++ pages/accueil.php
@@ -45,17 +45,144 @@
       <!-- About Section Content -->
       <div class="row">
         <div class="col-lg-4 ml-auto">
-          <p class="lead">Je suis Mamby Camara et je travaille en tant que développeur informatique. Mon parcours atypique en fait ma force puisque j’ai la chance de connaitre deux cultures différentes ce qui me permet une flexibilité mentale souvent requise dans l’informatique. </p>
+          <p class="lead">Je suis Mamby Camara et je travaille en tant que développeur informatique. Mon parcours atypique en fait ma
+             force puisque j’ai la chance de connaitre deux cultures différentes ce qui me permet une flexibilité mentale souvent requise dans l’informatique. </p>
         </div>
         <div class="col-lg-4 mr-auto">
-          <p class="lead">Je réponds à vos besoins et à vos demandes concernant toutes créations de sites internet (professionnels, sportif, commercial…). Après avoir échanger sur vos idées, votre parcours ainsi que votre personnalité, je m’engage créer un site internet à votre image. </p>
+          <p class="lead">Je réponds à vos besoins et à vos demandes concernant toutes créations de sites internet (professionnels, sportif,
+             commercial…). Après avoir échanger sur vos idées, votre parcours ainsi que votre personnalité, je m’engage créer un site internet à votre image. </p>
+        </div>
+      </div>
+
+      <!-- 1-->
+      <div class="row">
+        <div class="col-lg-10 ml-auto">
+          <p class="lead">Mon stage s’est déroulé à l’entreprise Europe Technologie, Sonimat, situé à Carquefou, du 29 juin au 21 août 2020.
+             J’étais sous la direction de Monsieur Tristan PEIGNET, qui travaille en tant que chef de la direction automatisme. La mission qui m’a été confié
+            a été de créer une application capable de se connecter à un générateur à travers un réseau filière et une adresse IP.
+           C’est mon tuteur de stage qui a choisi cette mission pour moi, car elle est directement en lien avec la formation que je suis à campus academy
+            c’est-à-dire conceptualiser et développer une application informatique. 
+          J’étais réellement motivé pour cette mission, car pour ce faire, je devais créer une application dans un nouveau langage que je n’ai pas encore
+           vu en formation (C SHARP) et qui est très utiliser de nos jours par plusieurs entreprises. J’étais donc persuadé que ce stage et plus particulièrement
+            la mission qui m’a été confié allait être réellement formateur pour moi. 
+         J’avais différentes attentes par rapport à cette première expérience. Dans un premier temps, je souhaitais découvrir le fonctionnement d’une entreprise,
+          les comportements à adopter dans le milieu professionnel, mais aussi les règles à respecter, ainsi que l’organisation d’une entreprise. De plus,
+           je souhaitais développer davantage mes connaissances dans le développement informatique, et plus particulièrement, me familiariser avec le langage C#. 
+       </p>
+        </div>
+      </div>  
+
+         <!-- 2-->
+        <div class="row">
+        <div class="col-lg-10 ml-auto">
+          <p class="lead"> L’entreprise que j’ai intégré est une entreprise robotique. Ils fabriquent et programment des machines industrielles pour des 
+        clients dans le milieu agroalimentaire, composites, packaging, plasturgie et textile. Ils peuvent par exemple dans le domaine alimentaire proposer
+         des solutions pour le parachèvement composite au travers de différents procédés de découpe ultrason ou soudure ultrason, ce qui permet d’offrir
+          de nombreux avantages pour le façonnage des produits composites. Ils sont de plus les premiers à avoir développé un système de soudure packaging
+           par ultrason dans le domaine de l’emballage. Il s’agit d’un procédé breveté de scellage ultrasons en continu afin d’améliorer la qualité de la soudure
+         et les cadences de la machine. Ils sont aussi spécialisés dans la soudure des matériaux thermoplastiques et proposent notamment des savoir-fair
+         e à ses clients dans différents domaines d’activités de la plasturgie, par exemple, l’automobile, le médicale, ou encore les biens de grande
+          consommation…. Pour finir, SONIMAT est connue pour la grande fiabilité de ses machines intégrant des procédés de soudure des thermoplastiques 
+          aux technologies infrarouge. 
+         L’entreprise Sonimat possède trois pôles, le premier à Carquefou, le second à Chatillon en Michaille et le dernier à Lencloître.  
+        Je vais me focaliser sur le pôle de Carquefou, qui possède six bureaux d’études différents. Un premier qui se centre sur le montage et le mise en service,
+         un second bureau d’étude en automatisme, un troisième en Service Après Vente, un quatrième qui s’occupe de développer les produits, un cinquième dans la 
+         gestion de projet en enfin le dernier dans l’étude mécanique (voir organigramme ci-joint). 
+        Le service que j’ai intégré lors de mon stage est le bureau d’étude automatisme.  Le rôle de ce service est d’entretenir les machines chez le client, 
+        que cela soit mécanique ou bien informatique. Le service s’occupe aussi du paramétrage des machines en fonction de la 
+        demande du client.
+        
+       </p>
+
+         </div>
+
+      </div>
+      <img class="img-fluid" src="img/sonimat.jpg" alt="">
+
+         <!-- 3-->
+      < <div class="row">
+        <div class="col-lg-10 ml-auto">
+          <p class="lead">Mon objectif de stage, était de permettre aux membres de l’entreprise de pouvoir se connecter et paramétrer les machines grâce à un code d’accès. J’ai travaillé en individuel sur cette application, avec parfois, l’aide de mon maitre de stage pour répondre à mes différentes questions. Pour ce faire, j’ai dû créer une application. Cette application devait permettre aux membres de l’entreprise de pouvoir se connecter avec leurs identifiants au générateur afin de pouvoir le paramétrer directement, grâce à un mot de passe et durant un délai illimité. 
+               Cette mission m’a été confié car auparavant, quand les membres de l’entreprise se connectaient sur le générateur, ils étaient automatiquement déconnectés au bout de dix minutes, ce qui obligeait l’utilisateur à se reconnecter parfois à plusieurs reprises. 
+               J’ai donc dû trouver une solution pour que la connexion soit permanente une fois connecter au générateur afin de faciliter le travail aux utilisateurs et ne pas risquer de perdre du temps entre chaque connexion. 
+               L’impact positif de cette application sur l’entreprise est donc de faire gagner du temps aux salariés, en ne s’identifiant pas à chaque connexion, mais aussi de leur faciliter l’accès aux paramétrages du générateur. La non-réussite de l’application aura pour conséquence de rester sur l’ancien modèle de l’application ce qui va générer une perte de temps et d’énergie pour les salariés. 
+          </p>
+        </div>
+      </div>  
+
+         <!-- 4-->
+       <div class="row">
+        <div class="col-lg-10 ml-auto">
+          <p class="lead">Pour réaliser cette application j’ai d’abord dû me former au langage C Sharp (C#). Durant les deux premières semaines de mon stage,
+             je me suis formé à l’aide d’une formation en ligne open classroom (voir lien ci-dessous). Je me suis ensuite concentré sur 
+             la création de l’application. J’ai commencé avec un travail de recherche pour trouver les outils adaptés à mon projet.
+              J’ai ensuite créé un powerpoint pour présenter l’application à mon chef de stage (voir power point ci-joint). Après validation
+               j’ai mis toutes les étapes de l’application en brouillon sur papier et j’ai pu commencer à développer mon application grâce à un 
+               logiciel «visuel studio » (windows form). J’ai pu effectuer une formation sur windows form afin de pouvoir utiliser les framworks 
+               propres à cette application. J’ai de plus réalisé un travail de recherche sur le réseau « modbus TCP/IP » pour pouvoir connecter 
+               l’adresse IP de l’ordinateur à l’adresse IP du générateur afin d’accéder ou de modifier les paramétrages de la machine (voir lien ci-dessous).
+               <br> <a href="http://www.simplymodbus.ca/FC01.htm"></a>
+               
+
+               Ce travail de recherche a été nécessaire afin de m’aider dans la réalisation de mon application. 
+                En parallèle, des réunions hebdomadaires étaient organisés tous les vendredis afin de faire le point sur 
+                les différents avancements des projets en cours. J’ai donc pu partager ma progression avec mes collègues,
+                 qui ont pu me donner leurs conseils lors de ces réunions. 
+                  Grâce à tout ce travail de recherche, j’ai pu réaliser mon application au délais donné. Néanmoins, j’ai rencontré lors de la
+                   réalisation de ce projet quelques difficultés, dû à mes connaissances, encore un peu limité, en langage C Sharp. 
+
+          </p>
+        </div>
+      </div>
+
+        <!-- 5-->
+       <div class="row">
+        <div class="col-lg-10 ml-auto">
+          <p class="lead">
+          Lors de la durée de mon stage, j’ai pu développer différentes compétences liées directement au monde de l’entreprise (soft skills),
+           telles que l’autonomie, comme j’ai dû la plupart du temps travailler en individuel. J’ai de plus pu développer ma méthode de recherche 
+           afin de résoudre les problèmes liés à ma mission. Travaillant avec un délai limité, j’ai aussi dû composer avec la gestion du temps 
+           ainsi que la gestion du stress et enfin j’ai dû m’adapter et répondre aux différents conseils voire critiques que je pouvais recevoir de la part de mes collègues. 
+          J’ai de plus développé des compétences liées directement au domaine de l’informatique (hard skills) : J’ai déjà pu apprendre un nouveau
+           langage que je ne connaissais pas, le C Sharp. J’ai de plus pu me servir des connaissances acquises lors de ma formation universitaire 
+           telles que l’esprit d’analyse qu’il faut pour aborder les problèmes et trouver une solution avant de commencer à développer une application.
+            J’ai aussi pu me servir des différentes connaissances apprises dans les langages informatiques afin de les réutiliser dans d’autres langages
+             même si les syntaxes sont différentes. Enfin j’ai pu me familiariser davantage avec les connaissances nécessaires pour développer une
+              application informatique (windows form, framworks, visual studio …).
+          Ce stage répond partiellement à mes attentes, car même si j’ai appris à développer une application de bout en bout, je me suis,
+           néanmoins, souvent trouvé en difficultés. En effet, étant en première année informatique, j’avais souvent besoin d’aide pour me
+            débloquer ou avancer et mon maître de stage ne pouvait pas tout le temps répondre à ma demande, étant régulièrement en déplacement
+             ou à gérer d’autres projets en cours. 
+
+               </p>
+        </div>
+      </div>
+
+        <!-- 6-->
+      <div class="row">
+        <div class="col-lg-10 ml-auto">
+          <p class="lead"> 
+          Ayant déjà eu des expériences professionnelles auparavant, cette nouvelle expérience n’a fait que me confirmer ce que j’avais déjà pu
+           rencontrer. A savoir que le monde de l’entreprise répond à une hiérarchie mais qu’il est nécessaire de pouvoir travailler en lien les
+            uns avec les autres pour pouvoir arriver à un objectif commun. 
+            J’espère pouvoir l’année prochaine continuer à développer ma posture de jeune professionnel en intégrant une nouvelle entreprise en 
+            alternance et me centrer peut-être davantage encore sur le développement d’application informatique afin de monter en compétences et
+             maitriser complètement ce domaine à plus long terme. 
+              Je pense avoir fait une bonne impression et avoir répondu aux attentes de mon maître de stage, puisqu’il a lui-même rédiger une lettre
+               de recommandation pour de futurs potentiels employeurs (voir ci-joint). 
+
+              En conclusion, ce stage a été très formateur pour moi et je remercie mon maître de stage Tristan PEIGNET d’avoir pu me suivre lors de
+               ces deux derniers mois et répondre aux questions que je pouvais me poser. Ce stage, bien qu’ayant eu parfois ses limites, me conforte
+                dans l’idée de devenir développeur informatique. 
+
+          </p>
         </div>
       </div>
 
       <!-- About Section Button -->
       <div class="text-center mt-4">
         <a class="btn btn-xl btn-outline-light" href="img/portfolio/cv mamby.pdf">
-          <i class="fas fa-download mr-2"></i>
+          <i class="fas fa-download m-2"></i>
           Télécharger mon CV!
         </a>
       </div>
